685c3a79bce8f0a8c582c09996417d0bb67b68d0
danielbatres <danielbatresuser@gmail.com>
2022-08-08 10:57:11 -0600

    Components TODO machine

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+// import './App.css';
+
+const todos = [
+  { text: "cut onion", completed: false },
+  { text: "take react intro course", completed: false },
+  { text: "cry lol", completed: false },
+  { text: "another todo", completed: false }
+];
+
+function App() {
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
+  );
+}
+
+export default App;
